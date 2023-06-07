97ff353f0decfb83051394f239ac9454118570bb
Ahaelgebaly <80677542+Ahaelgebaly@users.noreply.github.com>
2023-06-07 21:05:04 +0300

    Add files via upload

---
+++ lib/pages/feeds.dart
@@ -0,0 +1,117 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:discovery/screens/search.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:discovery/models/post.dart';
+import 'package:discovery/utils/constants.dart';
+import 'package:discovery/utils/firebase.dart';
+import 'package:discovery/widgets/indicators.dart';
+import 'package:discovery/widgets/userpost.dart';
+
+class Feeds extends StatefulWidget {
+  @override
+  _FeedsState createState() => _FeedsState();
+}
+
+class _FeedsState extends State<Feeds> with AutomaticKeepAliveClientMixin {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  int page = 5;
+  bool loadingMore = false;
+  ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    scrollController.addListener(() async {
+      if (scrollController.position.pixels ==
+          scrollController.position.maxScrollExtent) {
+        setState(() {
+          page = page + 5;
+          loadingMore = true;
+        });
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(
+          Constants.appName,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            fontFamily: 'Roboto'
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: RefreshIndicator(
+        color: Theme.of(context).colorScheme.secondary,
+        onRefresh: () =>
+            postRef.orderBy('timestamp', descending: true).limit(page).get(),
+        child: SingleChildScrollView(
+          physics: NeverScrollableScrollPhysics(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height-180,
+                child: FutureBuilder(
+                  future: postRef
+                      .orderBy('timestamp', descending: true)
+                      .limit(page)
+                      .get(),
+                  builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.hasData) {
+                      var snap = snapshot.data;
+                      List docs = snap!.docs;
+                      return ListView.builder(
+                        controller: scrollController,
+                        itemCount: docs.length,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          PostModel posts =
+                              PostModel.fromJson(docs[index].data());
+                          return Container(
+                            width: 329,
+                            height: 250,
+                            child: Padding(
+                              padding: EdgeInsets.only(right: 32,left: 32 , bottom: 20),
+                              child: UserPost(post: posts),
+                            ),
+                          );
+                        },
+                      );
+                    } else if (snapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return circularProgress(context);
+                    } else
+                      return Center(
+                        child: Text(
+                          'No Feeds',
+                          style: TextStyle(
+                            fontSize: 26.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
